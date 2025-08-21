1210163af7ef6a38de9494d598116b5ba3d2b58a
rohitadhikari04 <rohitadhikari0406@gmail.com>
2025-08-21 21:20:41 +0530

    Delete leaderboard-app directory

--- leaderboard-app/backend/src/models/ClaimHistory.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const claimHistorySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  points: { type: Number, required: true, min: 1, max: 10 },
-}, { timestamps: true });
-
-export default mongoose.model('ClaimHistory', claimHistorySchema);
